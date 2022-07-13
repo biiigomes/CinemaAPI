3a39699446ed44db528fec30ecdcfdc02c2a1891
badf <bianca.ferreira@gft.com>
2022-07-13 15:58:51 -0300

    building email service

--- UsuarioFuncs/Services/EmailService.cs
+++ UsuarioFuncs/Services/EmailService.cs
@@ -1,10 +1,42 @@
+
+using MailKit.Net.Smtp;
+using MimeKit;
+using UsuarioFuncs.Models;
+
 namespace UsuarioFuncs.Services
 {
     public class EmailService
     {
         public void EnviarEmail(string[] destinatario, string assunto, int userId, string code)
         {
-            
+            Mensagem mensagem = new Mensagem(destinatario, assunto, userId, code);
+            var mensagemDeEmail = CriaCorpoDoEMail(mensagem);
+            Enviar(mensagemDeEmail);
         }
-    }
+
+        private void Enviar(MimeMessage mensagemDeEmail)
+        {
+            using(var cliente = new SmtpClient())
+            {
+                try
+                {
+                    cliente.Connect("TODO");
+                    cliente.Send(mensagemDeEmail);
+                }
+            };
+        }
+
+        private MimeMessage CriaCorpoDoEMail(Mensagem mensagem)
+        {
+            var mensagemDeEmail = new MimeMessage();
+            mensagemDeEmail.From.Add(new MailboxAddress("ADICIONAR REMETENTE"));
+            mensagemDeEmail.To.AddRange(mensagem.Destinatario);
+            mensagemDeEmail.Subject = mensagem.Assunto;
+            mensagemDeEmail.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = mensagem.Conteudo
+            };
+            return mensagemDeEmail;
+        }
+    } 
 }
